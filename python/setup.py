94cd16ba69ed7822a4d75a54ae30258c38c34589
Robert Bartel <robert.bartel@noaa.gov>
2020-09-16 10:22:37 -0500

    Adjust setup.py and how it handles version.
    Adjusting setup.py to pre-set the package __version__ variable to
    prevent code analysis problems, but also raise an error if the value is
    not read and re-set properly.

--- python/setup.py
+++ python/setup.py
@@ -6,7 +6,13 @@ try:
 except:
     long_description = 'HY_Features python package.'
 
-exec(open('hypy/_version.py').read())
+# Set __version__ to prevent analysis check issues, but later make sure it gets read properly
+__version__ = ''
+_version_file = 'hypy/_version.py'
+exec(open(_version_file).read())
+# Raise error if __version__ is still the pre-set empty string
+if __version__ == '':
+    raise RuntimeError('Failed to read package "__version__" from package version file "{}".'.format(_version_file))
 
 setup(
     name='hypy',
