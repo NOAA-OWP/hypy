74ce0ed95c79b00dd3b3b2d102cf32fa9be9d08e
John David Mattern <jdmattern@localhost.localdomain>
2020-09-17 11:12:28 -0400

    initial implementation of nexus class

--- python/hypy/nexus.py
+++ python/hypy/nexus.py
@@ -1,21 +1,49 @@
-"""
-    HY Features Nexus class
-"""
-
 class Nexus():
     """
-
+    Implementation of the HY Features Nexus concept.
     """
-    __slots__ = ['_id']
-    def __init__(self, id: str):
-        """
+    __slots__ = ["_id", "_to_id"]
 
+    def __init__(self, nexus_id: str, to_id: str):
         """
-        self._id = id
+        Set the nexus identity and transform the raw params into dataframes
+        
+        Parameters
+        ----------
+        nexus_id: str
+            The identifier string for this nexus
+        to_id:
+            The catchment identifier that is immediately downstream of this nexus
+              
+        """
+        self._id = nexus_id
+        self._to_id = to_id
+        # TODO: Consider a connected from
 
     @property
     def id(self) -> str:
-        """
-
+        """Return nexus identifier
+        Returns
+        -------
+            str
+                nexus identifier
         """
         return self._id
+
+    @property
+    def to_id(self) -> str:
+        """Return catchment identifier that is immediately downstream of this nexus
+
+        Returns
+        -------
+            str
+                downstream catchment identifier
+        """
+        return self._to_id
+
+class Observation_Point(Nexus):
+    """
+    Subclass of Nexus to define a hydrological observation point that could connect to a data source
+    from a USGS gaging station as an example.
+    """
+    pass
--- python/hypy/test/test_nexus.py
+++ python/hypy/test/test_nexus.py
@@ -1,6 +1,13 @@
 import pytest
+from typing import Generator
+from pathlib import Path
+import json
 
-from hypy import Nexus
+from hypy import Nexus, Observation_Point
+
+"""
+    Test suite for Nexus class
+"""
 
 @pytest.fixture
 def nexus():
@@ -8,11 +15,33 @@ def nexus():
         Nexus object to test
     """
     id = 'nex-test'
-    nexus = Nexus(id)
+    to_id = 'downstream_catchment'
+    nexus = Nexus(id, to_id)
     yield nexus
+
+@pytest.fixture
+def observation_point():
+    """
+        Observation_Point object to test
+    """
+    id = 'obs-point-test'
+    to_id = 'downstream_catchment'
+    observation_point = Observation_Point(id, to_id)
+    yield observation_point
 
 def test_nexus(nexus):
     """
         Test proper construction of nexus
     """
     assert nexus.id == 'nex-test'
+
+    assert nexus.to_id == 'downstream_catchment'
+
+def test_observation_point(observation_point):
+    """
+        Test proper construction of observation_point
+    """
+    assert observation_point.id == 'obs-point-test'
+
+    assert observation_point.to_id == 'downstream_catchment'
+
