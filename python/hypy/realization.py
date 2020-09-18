6bd903071189e9ae74c9569c45b8ca5edfd4e66f
John David Mattern <jdmattern@localhost.localdomain>
2020-09-18 11:00:25 -0400

    initial implementation of realization class

--- python/hypy/realization.py
+++ python/hypy/realization.py
@@ -1,27 +1,52 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from pandas import Series
+from typing import Optional
 
 class Realization(object):
     """
+    Implementation of the HY Features Realization concept.
 
     """
-    __slots__ = ['_id']
-    def __init__(self, id):
-        """
 
+    __slots__ = ['_id', '_catchment_id']
+
+    def __init__(self, 
+                 realization_id: str,
+                 catchment_id: Optional[str] = None):
         """
-        self._id = id
-        pass
+        Set the realization identity and parameters
+
+        Parameters
+        ----------
+        realization_id: str
+            The identifier string for this realization
+        catchment_id:
+            The identifier string for the realized catchment, or ``None`` if it does not have one.
+        """
+        self._id = realization_id
+        self._catchment_id = catchment_id
 
     @property
     def id(self) -> str:
         """Return realization identifier
-
         Returns
         -------
             str
                 realization identifier
         """
         return self._id
+
+    @property
+    def catchment_id(self) -> Optional[str]:
+        """Return realization identifier
+        Returns
+        -------
+            str
+                The (optional) catchment identifier that the realization points to.
+        """
+        return self._catchment_id
+
+class Catchment_Area(Realization):
+    """
+    Subclass of Realization as a catchment area connecting the inflow and outflow of the catchment it realizes
+
+    """
+    pass
