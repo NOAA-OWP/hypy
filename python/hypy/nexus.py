372da3c5842f4f54d71830e4feeb72331c2cd093
John David Mattern <jdmattern@localhost.localdomain>
2020-09-18 16:21:25 -0400

    added receiving_catchments, contributing_catchments, and hydro_location to nexus class

--- python/hypy/nexus.py
+++ python/hypy/nexus.py
@@ -1,45 +1,94 @@
+from typing import List, Optional, Tuple, TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from .catchment import Catchment, Catchments_Collection
+
 class Nexus():
     """
     Implementation of the HY Features Nexus concept.
     """
-    __slots__ = ["_id", "_to_id"]
 
-    def __init__(self, nexus_id: str, to_id: str):
+    @classmethod
+    def _convert_collection_to_tuple(cls, collection: 'Catchments_Collection') -> Tuple['Catchment']:
         """
-        Set the nexus identity and transform the raw params into dataframes
+        Convenience method to accept a ``Catchments_Collection``, which is a union of several possible types, and to
+        output a tuple of catchments (or an empty tuple).
+        Parameters
+        ----------
+        collection: Catchments_Collection
+            A collection of catchment objects, which could be objects within certain containers, an empty container, or
+            a single catchment object itself.
+        Returns
+        -------
+        Tuple['Catchment']
+            A tuple containing the catchments included directly or in the container parameter object.
+        """
+        if isinstance(collection, list):
+            return tuple(collection)
+        elif isinstance(collection, tuple):
+            return collection
+        # Assuming type hinting is followed, the only thing this should leave is the single catchment
+        # TODO: consider whether any cases outside of type hint need to be addressed
+        else:
+            return (collection,)
+
+    __slots__ = ["_id", "_receiving_catchments", "_contributing_catchments", "_hydro_location"]
+
+    def __init__(self, 
+                 nexus_id: str, 
+                 hydro_location,
+                 receiving_catchments: 'Catchments_Collection' = tuple(),
+                 contributing_catchments: 'Catchments_Collection' = tuple()):   
+        """
+        Set the nexus identity and params
         
         Parameters
         ----------
         nexus_id: str
             The identifier string for this nexus
-        to_id:
-            The catchment identifier that is immediately downstream of this nexus
+        receiving_catchments: Catchments_Collection
+            The catchment object(s) that receive(s) water from this nexus
+        contributing_catchments: Catchments_Collection
+            The catchment object(s) that contribute(s) water to this nexus
               
         """
         self._id = nexus_id
-        self._to_id = to_id
-        # TODO: Consider a connected from
+        self._hydro_location = hydro_location
+        self._receiving_catchments = self._convert_collection_to_tuple(receiving_catchments) 
+        self._contributing_catchments = self._convert_collection_to_tuple(contributing_catchments) 
 
     @property
     def id(self) -> str:
         """Return nexus identifier
+
         Returns
         -------
-            str
-                nexus identifier
+        str
+            nexus identifier
         """
         return self._id
 
     @property
-    def to_id(self) -> str:
-        """Return catchment identifier that is immediately downstream of this nexus
+    def receiving_catchments (self) -> Tuple['Catchment']:
+        """Tuple of Catchment object(s) receiving water from nexus
 
         Returns
         -------
-            str
-                downstream catchment identifier
+        Tuple['Catchment']
+            Tuple of Catchment object(s) receiving water from nexus 
         """
-        return self._to_id
+        return self._receiving_catchments 
+
+    @property
+    def contributing_catchments (self) -> Tuple['Catchment']:
+        """Tuple of Catchment object(s) contributing water to nexus
+
+        Returns
+        -------
+        Tuple['Catchment']
+            Tuple of Catchment object(s) contributing water to nexus
+        """
+        return self._contributing_catchments 
 
 class Observation_Point(Nexus):
     """
